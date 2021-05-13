137b2e5d7202475846fcb8bde7d0503516ff98b9
Thanh <thanhnguyenvanpro123@gmail.com>
2021-05-13 16:01:07 +0700

    fix pagination when total pages < 3

--- resources/views/common/pagination.blade.php
+++ resources/views/common/pagination.blade.php
@@ -3,31 +3,44 @@
         <ul class="pagination">
        
             <li class="page-item"><a class="page-link @if ($paginator->currentPage() == 1) invalid-page-number @endif " href="{{$paginator->previousPageUrl()}}">{{ trans('localization.previous')}}</a></li>
-
-        @if ($paginator->currentPage() == 1)
-            <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
-        @elseif ($paginator->currentPage() == $paginator->lastPage())
-            <li class="page-item"><a class="page-link" href="{{$paginator->url($paginator->currentPage() - 2)}}">{{$paginator->currentPage() - 2}}</a></li>
-        @else
-            <li class="page-item"><a class="page-link" href="{{$paginator->previousPageUrl()}}">{{$paginator->currentPage() - 1}}</a></li>
-        @endif    
-
-        @if ($paginator->currentPage() == 1)
-            <li class="page-item"><a class="page-link" href="{{$paginator->nextPageUrl()}}">{{$paginator->currentPage() + 1}}</a></li>
-        @elseif ($paginator->currentPage() == $paginator->lastPage()) 
-            <li class="page-item"><a class="page-link" href="{{$paginator->previousPageUrl()}}">{{$paginator->currentPage() - 1}}</a></li>
-        @else
-            <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
-        @endif
         
-        @if ($paginator->currentPage() == 1)
-            <li class="page-item"><a class="page-link" href="{{$paginator->url($paginator->currentPage() + 2)}}">{{$paginator->currentPage() + 2}}</a></li>
-        @elseif ($paginator->currentPage() == $paginator->lastPage()) 
-            <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
-        @else
-            <li class="page-item"><a class="page-link" href="{{$paginator->nextPageUrl()}}">{{$paginator->currentPage() + 1}}</a></li>
-        @endif  
+        @if ($paginator->lastPage() >=3 )
+            @if ($paginator->currentPage() == 1)
+                <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
+            @elseif ( ($paginator->currentPage() == $paginator->lastPage()) && ($paginator->lastPage() >= 3) )
+                <li class="page-item"><a class="page-link" href="{{$paginator->url($paginator->currentPage() - 2)}}">{{$paginator->currentPage() - 2}}</a></li>
+            @else
+                <li class="page-item"><a class="page-link" href="{{$paginator->previousPageUrl()}}">{{$paginator->currentPage() - 1}}</a></li>
+            @endif    
 
+            @if ($paginator->currentPage() == 1)
+                <li class="page-item"><a class="page-link" href="{{$paginator->nextPageUrl()}}">{{$paginator->currentPage() + 1}}</a></li>
+            @elseif ($paginator->currentPage() == $paginator->lastPage()) 
+                <li class="page-item"><a class="page-link" href="{{$paginator->previousPageUrl()}}">{{$paginator->currentPage() - 1}}</a></li>
+            @else
+                <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
+            @endif
+            
+            @if ($paginator->currentPage() == 1)
+                <li class="page-item"><a class="page-link" href="{{$paginator->url($paginator->currentPage() + 2)}}">{{$paginator->currentPage() + 2}}</a></li>
+            @elseif ($paginator->currentPage() == $paginator->lastPage()) 
+                <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
+            @else
+                <li class="page-item"><a class="page-link" href="{{$paginator->nextPageUrl()}}">{{$paginator->currentPage() + 1}}</a></li>
+            @endif  
+        @else 
+            @if ($paginator->currentPage() == 1)
+                <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
+            @else
+                <li class="page-item"><a class="page-link" href="{{$paginator->previousPageUrl()}}">{{$paginator->currentPage() - 1}}</a></li>
+            @endif    
+
+            @if ($paginator->currentPage() != 1)
+                <li class="page-item"><a class="page-link invalid-page-number" href="{{$paginator->url($paginator->currentPage())}}">{{$paginator->currentPage()}}</a></li>
+            @else
+                <li class="page-item"><a class="page-link" href="{{$paginator->nextPageUrl()}}">{{$paginator->currentPage() + 1}}</a></li>
+            @endif    
+        @endif    
             <li class="page-item"><a class="page-link @if (!$paginator->hasMorePages()) invalid-page-number @endif " href="{{$paginator->nextPageUrl()}}">{{ trans('localization.next')}}</a></li>
     
         </ul>
